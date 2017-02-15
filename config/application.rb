848250b43fa44b86aabdbd9440583aaf7d615a3d
Ionut Boanta <ionut.boanta@crisser.ro>
2017-02-15 10:35:44 +0000

    w/e

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module FinanceTracker
   class Application < Rails::Application
-    config.web_console.whitelisted_ips = '109.98.231.19'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
