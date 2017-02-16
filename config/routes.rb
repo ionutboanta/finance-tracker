a31791289144f36fa8aad3011626569e694669e2
Ionut Boanta <ionut.boanta@crisser.ro>
2017-02-16 12:42:52 +0000

    friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 root 'welcome#index'
 get 'my_portofolio', to: 'users#my_portofolio'
 get 'search_stocks', to: 'stocks#search'
+get 'my_friends', to: "users#my_friends"
 
 
 
